9dfc32b63603f91fd473d64c98470ae54f2a76b6
Carlos Vazquez <carlosvazquezmurillo@gmail.com>
2017-09-01 22:31:07 -0500

    Init commit

---
+++ design-patterns-in-php/01/index.php
@@ -0,0 +1,6 @@
+<?php 
+require __DIR__ . '/vendor/autoload.php';
+use App\Demo;
+
+$saludo = new Demo;
+$saludo->hola();
---
+++ design-patterns-in-php/01/src/demo.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+
+use App\Demo;
+
+class Demo {
+	public function hola() {
+		var_dump('hola mundoaaa');
+	}
+}
